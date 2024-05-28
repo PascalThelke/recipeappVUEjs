6e69c3b429f15eb68139f399a0d8c2b49c4a31e7
Pascal Thelke <p.thelke@googlemail.com>
2024-05-28 17:04:35 +0200

    first component implemented

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <input placeholder="Suche..." type="text">
+    </header>
+ 
+</template>
+
+<script>
+    export default {
+        name: 'header',
+    }
+</script>
+
+<style scoped>
+Header {
+    background-color: #f4bc39;
+    height: 120px;
+    display: flex;
+    align-items: flex-end;
+    padding: 16px;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+</style>
